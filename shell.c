c4df78cc0b717ed59096a8ebea6d1fcb19b52321
chloe0524 <chloco31@gmail.com>
2023-12-11 10:07:00 -0800

    version wth debug and fork to be added

---
+++ shell.c
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/*
+ *
+ *
+ */
+
+int file_exist(char *file)
+{
+	struct stat st;
+
+	if (stat(file, &st) == 0)
+	{
+		printf(" FOUND\n");
+		return (0);
+	}
+	else
+	{
+		printf("%s NOT FOUND\n", file);
+		perror("Error");
+		return (-1);
+	}
+
+}
+
+int execute_command(char *cmd)
+{
+	if (file_exist(cmd) == -1)
+		return (-1);
+	printf("execution %s\n", cmd);
+	return(0);
+}
+
+int main (int argc, char **argv)
+{
+	char *input_buffer;
+	size_t size_allocated;
+	size_t char_read;
+	int i;
+
+	do {
+		printf("#cisfun$ ");
+		char_read = getline(&input_buffer, &size_allocated, stdin);
+
+		if (char_read < 0)
+		{
+			fprintf(stderr, "getline failed\n");
+			free(input_buffer);
+			return(-1);
+		}
+
+		if (char_read == SIZE_MAX)
+		{
+			free(input_buffer);
+			putchar('\n');
+			return(-2);
+		}
+		/*for (i = 0; i < char_read; i++)
+		  printf("%d ", input_buffer[i]);
+		  putchar('\n');*/
+		input_buffer[char_read - 1] = 0;
+		if (execute_command(input_buffer) == -1)
+			printf("echec execute_command\n");
+
+	} while (strcmp(input_buffer, "exit") != 0);
+	free(input_buffer);
+	return (0);
+}
