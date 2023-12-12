5fb92e68fc5d0aa174b6e4f152f0ebcea9d8ffeb
chloe0524 <chloco31@gmail.com>
2023-12-12 03:54:34 -0800

    added prototype functions for execute if exists and main

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int file_exist(char *file);
+int execute_command(char *cmd);
+int main(int argc, char **argv);
+
+#endif
