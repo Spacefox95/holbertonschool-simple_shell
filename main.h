02ecdde3e7da280178411516d2f033f8b4512655
Spacefox95 <nathan.raynal@gmail.com>
2023-12-12 05:05:01 -0800

    AddingStrtok

--- main.h
+++ main.h
@@ -1,4 +1,20 @@
 #ifndef SIMPLE_SHELL
 #define SIMPLE_SHELL
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
+/* PROTOTYPES */
+
+int main (int argc, char **argv);
+int file_exist(char *file);
+int execute_command(char *cmd);
+
+
+#endif
--- shell.c
+++ shell.c
@@ -4,39 +4,74 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 /*
  *
  *
  */
 
-/*int file_exist(char *file)
+int file_exist(char *file)
 {
 	struct stat st;
 
 	if (stat(file, &st) == 0)
 	{
-		printf(" FOUND\n");
 		return (0);
 	}
 	else
 	{
-		printf("%s NOT FOUND\n", file);
 		perror("Error");
-		return (-1);
+		return (1);
 	}
 
 }
 
 int execute_command(char *cmd)
 {
-	if (file_exist(cmd) == -1)
-		return (-1);
-	printf("execution %s\n", cmd);
-	return(0);
-}*/
+	pid_t child_pid;
+	int status;
+	int i = 0;
+	char *token, **args;
 
-int main (int argc, char **argv)
+	if (file_exist(cmd) == 1)
+		return (1);
+
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	if (child_pid == 0)
+	{
+		token = strtok(cmd, " ");
+		*args = malloc(sizeof(char *) * 2);
+
+		if (args == NULL)
+		{
+			perror("Error");
+			return (1);
+		}
+		while (token != NULL)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+		execve(args[0], args, NULL);
+		perror("Error");
+		return (1);
+	}
+	else
+	{
+		wait(&status);
+	}
+	return (0);
+}
+
+int main(int argc, char **argv)
 {
 	char *input_buffer;
 	size_t size_allocated;
@@ -44,33 +79,25 @@ int main (int argc, char **argv)
 	int i;
 
 	do {
-		printf("$ ");
+		printf("#cisfun$ ");
 		char_read = getline(&input_buffer, &size_allocated, stdin);
 
 		if (char_read < 0)
 		{
 			fprintf(stderr, "getline failed\n");
 			free(input_buffer);
-			return(-1);
+			return (1);
 		}
 
 		if (char_read == SIZE_MAX)
 		{
 			free(input_buffer);
 			putchar('\n');
-			return(-2);
+			return (1);
 		}
-		if (char_read != -1)
-		{
-			printf("%s\n", input_buffer);
-			free(input_buffer);
-		}
-		/*for (i = 0; i < char_read; i++)
-		  printf("%d ", input_buffer[i]);
-		  putchar('\n');*/
 		input_buffer[char_read - 1] = 0;
-		/*if (execute_command(input_buffer) == -1)
-			printf("echec execute_command\n");*/
+		if (execute_command(input_buffer) == 1)
+			printf("echec execute_command\n");
 
 	} while (strcmp(input_buffer, "exit") != 0);
 	free(input_buffer);
