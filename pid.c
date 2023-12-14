4be4af25f772260c47579cce0a672965bb1598bf
Spacefox95 <nathan.raynal@gmail.com>
2023-12-14 07:37:57 -0800

    TryingToRevertCommit

---
+++ pid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_pid;
+
+    my_pid = getpid();
+    printf("%u\n", my_pid);
+    return (0);
+}
