3493579cd1ad01a742fdaf0bfb79d3221be1d05c
Spacefox95 <nathan.raynal@gmail.com>
2023-12-14 00:35:38 -0800

    Update_my_branch

--- shell.c
+++ shell.c
@@ -1,15 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-/*
- *
- *
+#include "main.h"
+
+/**
+ * fill_args - Tokenizes a string into an array of arguments.
+ * @input_buffer: The string to tokenize.
+ * @argv: The array to fill with the tokenized arguments.
+ * Return: The number of arguments filled into the array.
  */
+
+
+int fill_args(char *input_buffer, char ***argv)
+{
+	char *token;
+	int ctr = 0;
+
+	*argv = NULL;
+
+	token = strtok(input_buffer, " ");
+	while (token != NULL)
+	{
+		/* ctr+1 : nombre allocs taille sizeof */
+		(*argv) = realloc((*argv), (ctr + 1) * sizeof(char *));
+		(*argv)[ctr] = token;
+		ctr++;
+		token = strtok(NULL, " ");
+	}
+	(*argv) = realloc((*argv), (ctr + 1) * sizeof(char *));
+	(*argv)[ctr] = NULL;
+	return (ctr);
+}
+
+/**
+ * file_exist - Checks if a file exists.
+ * @file: The name of the file to check.
+ * Return: 0 if the file exists, 1 otherwise.
+ */
+
 
 int file_exist(char *file)
 {
@@ -21,18 +46,24 @@ int file_exist(char *file)
 	}
 	else
 	{
-		perror("Error");
+		perror("./shell");
 		return (1);
 	}
 
 }
 
-int execute_command(char *cmd)
+/**
+ * execute_command - Executes a command.
+ * @argv: The array of arguments for the command.
+ * Return: 0 on success, 1 on failure.
+ */
+
+
+int execute_command(char *argv[])
 {
 	pid_t child_pid;
 	int status;
-	char *token;
-	char *delim = " ";
+	char *cmd = argv[0];
 
 	if (file_exist(cmd) == 1)
 		return (1);
@@ -45,12 +76,7 @@ int execute_command(char *cmd)
 	}
 	if (child_pid == 0)
 	{
-		token = strtok(cmd, delim);
-		while (token != NULL)
-		{
-			token = strtok(NULL, delim);
-		}
-		execve(cmd, (char *[]) {NULL}, (char *[]) {NULL});
+		execve(cmd, argv, NULL);
 		perror("Error");
 		return (1);
 	}
@@ -61,23 +87,25 @@ int execute_command(char *cmd)
 	return (0);
 }
 
-int main(int argc, char **argv)
+/**
+ * main - The main function of the shell.
+ *
+ * Return: 0 on success, 1 on failure.
+ */
+
+int main(void)
 {
-	char *input_buffer;
+	char *input_buffer = NULL;
+	char **myargv = NULL;
 	size_t size_allocated;
 	size_t char_read;
-	int i;
 
 	do {
 		printf("#cisfun$ ");
 		char_read = getline(&input_buffer, &size_allocated, stdin);
 
-		if (char_read < 0)
-		{
-			fprintf(stderr, "getline failed\n");
-			free(input_buffer);
-			return (1);
-		}
+		if (char_read == 1)
+			continue;
 
 		if (char_read == SIZE_MAX)
 		{
@@ -86,10 +114,16 @@ int main(int argc, char **argv)
 			return (1);
 		}
 		input_buffer[char_read - 1] = 0;
-		if (execute_command(input_buffer) == 1)
-			printf("echec execute_command\n");
+		fill_args(input_buffer, &myargv);
 
-	} while (strcmp(input_buffer, "exit") != 0);
+		if (execute_command(myargv) == 1)
+		{
+			printf("echec execute_command\n");
+			free(myargv);
+		}
+
+	} 
+	while (strcmp(input_buffer, "exit") != 0);
 	free(input_buffer);
 	return (0);
 }
