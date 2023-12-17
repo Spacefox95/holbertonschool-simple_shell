f786d84dd971095fc77e54d3547e02067c4b7ae3
chloe0524 <chloco31@gmail.com>
2023-12-17 03:12:36 -0800

    function to fill argv_s

---
+++ fill_argv.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * fill_args - Tokenizes a string into an array of arguments.
+ * @input_buffer: The string to tokenize.
+ * Return: The number of arguments filled into the array.
+ */
+
+
+char **fill_args(char *input_buffer)
+{
+	int i = 0;
+	char *token;
+	char **argv = NULL;
+
+	token = strtok(input_buffer, " ");
+	while (token)
+	{
+		argv = realloc(argv, (i + 1) * sizeof(char *));
+		argv[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+
+	argv = realloc(argv, (i + 1) * sizeof(char *));
+	argv[i] = NULL;
+	return (argv);
+}
