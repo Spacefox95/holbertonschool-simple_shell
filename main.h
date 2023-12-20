9701e96b7302907fcff146bd6fa96f6d36c09f46
chloe0524 <chloco31@gmail.com>
2023-12-20 01:16:02 -0800

    added global var

--- main.h
+++ main.h
@@ -10,24 +10,19 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+
 /* GLOBAL VARIABLE */
 
 extern char **environ;
 
-/* LINKED LIST STRUCT */
-
-struct node {
-	char *dir;
-	struct node *next;
-};
 
 /* PROTOTYPES */
 
 int find_cmd_path(char *cmd, char *work_buffer);
 int file_exist(char *file);
-int execute_command(char *argv[], char *envp[]);
+int execute_command(char *argv[]);
 char **fill_args(char *input_buffer);
-void print_env(char *envp[]);
+void print_env(void);
 int shell_error(void);
 char *_getenv(const char *name);
 
