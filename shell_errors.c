7ae9f3b67d628a1094aefeba119d3830ed6c80de
chloe0524 <chloco31@gmail.com>
2023-12-21 09:28:34 -0800

    testing errno to see if its what is expected

--- shell_errors.c
+++ shell_errors.c
@@ -3,11 +3,20 @@
 /**
  * shell_error - print default error message
  *
+ * @cmd: command
+ * @err: error
+ *
  * Return: void
  */
 
-int shell_error(void)
+int shell_error(char *cmd, int err)
 {
-	perror("./shell - not found");
+	if (cmd == NULL)
+		fprintf(stderr, "./hsh: %d: %s: %s\n", err, cmd, strerror(err));
+	else if (err == 127)
+		fprintf(stderr, "./hsh: %d: %s: %s\n", 1, cmd, "not found");
+	else
+		fprintf(stderr, "./hsh: %d: %s: %s\n", 1, cmd, strerror(err));
+
 	return (EXIT_FAILURE);
 }
