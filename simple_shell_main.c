98fee0625da8673c27bc16b7f8fc4b4a4c11cc6c
chloe0524 <chloco31@gmail.com>
2023-12-22 12:34:53 -0800

    changed comments

--- simple_shell_main.c
+++ simple_shell_main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - The main function of the shell.
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ * Return: EXIT_SUCCESS on success or return code > 0 if error.
  */
 
 int main(void)
@@ -11,15 +11,14 @@ int main(void)
 	size_t size_allocated;
 	int char_read, ret = EXIT_SUCCESS;
 
-	signal(SIGINT, SIG_IGN); /* Ignore the signal of process */
+	signal(SIGINT, SIG_IGN); /* ignore ctrl-C signal */
 	do {
-		if (isatty(STDIN_FILENO)) /* Check interactive mode */
+		if (isatty(STDIN_FILENO))
 			printf("\033[0;39m#simple_shell(%d)$ ", getpid());
-		/* Display the prompt and the current process */
 
-		fflush(stdin); /* Update stream */
+		fflush(stdin);
 		char_read = getline(&input_buffer, &size_allocated, stdin);
-		/* Read the line from stream */
+
 		if (char_read == 1)
 			continue;
 		if (char_read == EOF)
@@ -27,11 +26,11 @@ int main(void)
 			free(input_buffer);
 			if (isatty(STDIN_FILENO))
 				putchar('\n');
-			return (ret);   /* Exit shell with Ctrl-D */
+			return (ret); /* exit shell with ctrl-D or | */
 		}
 
-		input_buffer[char_read - 1] = 0; /* Overwrite \n */
-		if (strncmp(input_buffer, "env", 3) == 0) /* Print the environnement */
+		input_buffer[char_read - 1] = 0; /* overwrite \n */
+		if (strncmp(input_buffer, "env", 3) == 0)
 		{
 			print_env();
 			continue;
@@ -39,12 +38,11 @@ int main(void)
 		if (strncmp(input_buffer, "exit", 4) == 0)
 		{
 			free(input_buffer);
-			return (ret);  /* Exit the shell with "exit" */
+			return (ret); /* ret= return code of last cmd before exit */
 		}
-		myargv = fill_args(input_buffer); /* Tokenize the input buffer */
+		myargv = fill_args(input_buffer);
 		if (myargv[0] != NULL)
 			ret = execute_command(myargv);
-		/* Execute the command associated to the token */
 		free(myargv);
 	} while (1);
 }
